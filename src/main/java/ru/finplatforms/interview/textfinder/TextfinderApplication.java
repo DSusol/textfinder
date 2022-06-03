0ea119e01ba9dbaa2c3313f776b75bea00a3cbd7
Dmitry Susol <dmitry.a.susol@gmail.com>
2022-06-03 11:07:06 +0300

    project initialization

---
+++ src/main/java/ru/finplatforms/interview/textfinder/TextfinderApplication.java
@@ -0,0 +1,13 @@
+package ru.finplatforms.interview.textfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextfinderApplication.class, args);
+    }
+
+}
